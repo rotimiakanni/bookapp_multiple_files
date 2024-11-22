e4f00bebb920f05a52e924f9fd8bdaf5df36514b
Adesayo M Labaeka <labaekaadesayo@gmail.com>
2024-11-22 03:58:08 +0000

    Ran unit tests for book endpoints

--- routers/books.py
+++ routers/books.py
@@ -10,25 +10,25 @@ from crud.books import book_crud
 
 book_router = APIRouter()
 
-@book_router.get("/{book_id}")
-def get_book(book_id: int):
-    return {"message": "success", "data": book_crud.get_book(book_id)}
+@book_router.get("/{value}")
+def get_book(value: str):
+    return {"message": "success", "data": book_crud.get_book(value)}
 
 @book_router.get("/")
 def get_books():
     return {"message": "success", "data": book_crud.get_books()}
 
-@book_router.post("/")
+@book_router.post("/", status_code=201)
 def create_book(payload: BookCreate):
     new_book = book_crud.create_book(payload)
     return {"message": "success", "data": new_book}
 
-@book_router.put("/{book_id}")
-def update_book(book_id: int, payload: BookUpdate):
-    book: Optional[Book] = book_crud.get_book(book_id)
+@book_router.put("/{value}")
+def update_book(value: str, payload: BookUpdate):
+    book: Optional[Book] = book_crud.get_book(value)
     updated_book = book_crud.update_book(book, payload)
     return {"message": "success", "data": updated_book}
 
-@book_router.delete("/{book_id}")
-def delete_book(book_id: int):
-    return book_crud.delete_book(book_id)
+@book_router.delete("/{value}", status_code=204)
+def delete_book(value: str):
+    return book_crud.delete_book(value)
